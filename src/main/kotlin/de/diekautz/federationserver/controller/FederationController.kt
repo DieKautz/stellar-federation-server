84451e6f8c641a32574c4b7b133913ed6d0bc202
DieKautz <malte@diekautz.dev>
2021-09-01 17:29:01 +0200

    refactored error handling in federation endpoint
    removed debug /all endpoint
    
    Signed-off-by: DieKautz <malte@diekautz.dev>

--- src/main/kotlin/de/diekautz/federationserver/controller/FederationController.kt
+++ src/main/kotlin/de/diekautz/federationserver/controller/FederationController.kt
@@ -1,10 +1,10 @@
 package de.diekautz.federationserver.controller
 
 import de.diekautz.federationserver.model.FederationAddress
+import de.diekautz.federationserver.model.FederationError
 import de.diekautz.federationserver.service.FederationService
 import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.MissingServletRequestParameterException
 import org.springframework.web.bind.annotation.*
 import javax.servlet.http.HttpServletRequest
@@ -16,23 +16,19 @@ class FederationController(private val service: FederationService) {
     private val tomlContent = this.javaClass.classLoader.getResource("stellar.toml").readText()
     private val log = LoggerFactory.getLogger(this.javaClass)
 
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(NoSuchElementException::class)
-    fun handleNotFound(e: NoSuchElementException): ResponseEntity<String> =
-        ResponseEntity("{\"error\":\"${e.message}\"}", HttpStatus.NOT_FOUND)
+    fun handleNotFound(e: NoSuchElementException) = FederationError(e.message ?: "Error")
 
-    @ExceptionHandler(IllegalArgumentException::class)
-    fun handleBadRequest(e: IllegalArgumentException): ResponseEntity<String> =
-        ResponseEntity("{\"error\":\"${e.message}\"}", HttpStatus.BAD_REQUEST)
-
-    @ExceptionHandler(MissingServletRequestParameterException::class)
-    fun handleEx(e: MissingServletRequestParameterException): ResponseEntity<String> =
-        ResponseEntity("{\"error\":\"${e.message}\"}", HttpStatus.BAD_REQUEST)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(value = [IllegalArgumentException::class, MissingServletRequestParameterException::class])
+    fun handleBadRequest(e: IllegalArgumentException) = FederationError(e.message ?: "Error")
 
     @GetMapping("/federation")
     fun getFedAddr(@RequestParam("type") type: String, @RequestParam("q") query: String, response: HttpServletResponse, request: HttpServletRequest): FederationAddress {
         when(type) {
             "name" -> {
-                log.info("Got name-type query for '$query' from ${request.remoteAddr}")
+                log.info("Got name-type query for '$query'")
                 response.setHeader("Access-Control-Allow-Origin", "*")
                 return service.getByFedAddress(query)
             }
@@ -42,29 +38,10 @@ class FederationController(private val service: FederationService) {
         }
     }
 
-    @GetMapping("/federation/all")
-    fun getAll(): Collection<FederationAddress> {
-        return service.getAddresses()
-    }
-
     @GetMapping("/.well-known/stellar.toml")
     fun serveStellarToml(response: HttpServletResponse) {
         response.setHeader("Access-Control-Allow-Origin", "*")
         response.contentType = "text/plain"
         response.outputStream.print(tomlContent)
     }
-
-    /*@PostMapping("/edit")
-    fun createFedAddr(@RequestBody address: FederationAddress): FederationAddress = service.createFedAddress(address)
-
-    @PutMapping("/edit")
-    fun updateFedAddr(
-        @RequestBody address: FederationAddress): FederationAddress
-            = service.updateFedAddress(address)
-
-    @DeleteMapping("/edit/{addr}")
-    fun deleteFedAddr(@PathVariable addr: String): ResponseEntity<Any?> {
-        service.deleteFedAddress(addr)
-        return ResponseEntity("{\"success\":\"Federation address successfully deleted!\"}", HttpStatus.NO_CONTENT)
-    }*/
 }
---
+++ src/main/kotlin/de/diekautz/federationserver/model/FederationError.kt
@@ -0,0 +1,3 @@
+package de.diekautz.federationserver.model
+
+data class FederationError(val error: String)
