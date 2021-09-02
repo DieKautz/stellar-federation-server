8283e4998bee045c841f9311410b2b845646290b
DieKautz <malte@diekautz.dev>
2021-09-02 02:29:07 +0200

    added memo size validation
    Signed-off-by: DieKautz <malte@diekautz.dev>

--- src/main/kotlin/de/diekautz/federationserver/service/FederationService.kt
+++ src/main/kotlin/de/diekautz/federationserver/service/FederationService.kt
@@ -45,18 +45,40 @@ class FederationService(private val dataSource: FederationAddressDataSource) {
 
 
     private fun validateAddress(address: FederationAddress) {
-        if (!address.stellarAddress.isValidFedAddress() || !address.accountId.isValidPubKey()) {
-            throw IllegalArgumentException("Address or account id invalid!")
+        if (!address.stellarAddress.isValidFedAddress()) {
+            throw IllegalArgumentException("Stellar address is invalid!")
         }
-        if (address.memoType != MemoType.NONE) {
-            if (address.memo.isNullOrBlank()) {
-                throw IllegalArgumentException("Memo may not be empty when type is specified!")
-            } else if (address.memoType == MemoType.HASH && !address.memo.matches(Regex("^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)?\$"))) {
-                throw IllegalArgumentException("Memo hash is invalid! (expected base64)")
+        if (!address.accountId.isValidPubKey()) {
+            throw IllegalArgumentException("Account id is invalid!")
+        }
+        if (address.memoType == MemoType.NONE) {
+            if (address.memo != null) {
+                throw IllegalArgumentException("Memo type has to match content!")
             }
         } else {
-            if (address.memo != null) {
-                throw IllegalArgumentException("Memo Type has to match content!")
+            if (address.memo.isNullOrBlank()) {
+                throw IllegalArgumentException("Memo may not be empty when type is specified!")
+            }
+            when(address.memoType) {
+                MemoType.TEXT -> {
+                    if (address.memo.encodeToByteArray().size > 28) {
+                        throw IllegalArgumentException("Memo is too large for type!")
+                    }
+                }
+                MemoType.HASH -> {
+                    if(!address.memo.matches(Regex("^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)?\$"))){
+                        throw IllegalArgumentException("Memo hash is invalid! (expected base64)")
+                    }
+                    val fillerCount = address.memo.count { it == '=' }
+                    if ((address.memo.count() - fillerCount)*3/4 > 32) {
+                        throw IllegalArgumentException("Memo hash is too big!")
+                    }
+                }
+                MemoType.ID -> {
+                    if (address.memo.toULongOrNull() == null) {
+                        throw IllegalArgumentException("Memo id should be 64-bit unsigned integer.")
+                    }
+                }
             }
         }
     }
